684b3d089398e8781b2c667f4dec0c1b3540e69e
Chia Yu <69720387+kk98033@users.noreply.github.com>
2021-12-11 22:33:25 +0800

    add_world_page_and_fix_bugs

---
+++ JS/create_table.js
@@ -0,0 +1,44 @@
+function tableCreate(){
+    const worldDic = JSON.parse(localStorage.getItem("worldDic")); 
+
+    const width = window.screen.width;
+    const body = document.getElementById("worldTable");
+    
+    tbl = document.createElement('table');
+    if(width <= 600){
+        tbl.style.fontSize = '10px';
+    }else{
+        tbl.style.fontSize = '20px';
+    }
+    tbl.style.width = '100%';
+    tbl.style.textAlign = 'center';
+    tbl.style.border = '1px solid black';
+
+    var tags = ["State", "Total Cases", "New Cases", "Total Deaths", "New Deaths", "Total Recovered", "New Recovered"]
+    var states = ['World', 'Asia', 'Europe', 'North_America', 'South_America', 'Africa', 'Oceania']
+    for(let i = 0; i < 8; i++){
+            const tr = tbl.insertRow();
+            for (let j = 0; j < 7; j++){
+                const td = tr.insertCell();
+                if(i === 0){
+                    td.appendChild(document.createTextNode(tags[j])); // top tags
+                    td.style.border = '1px solid black';
+                }else{
+                    if(j === 2 || j === 4){
+                        td.style.color = 'red';
+                    }
+                    if(j === 6){
+                        td.style.color = 'green';
+                    }
+                    if(j === 0){
+                        td.appendChild(document.createTextNode(states[i-1])); // state names
+                        td.style.border = '1px solid black';
+                    }else{
+                        td.appendChild(document.createTextNode(worldDic[states[i-1]][j-1])); // datas
+                        td.style.border = '1px solid black';
+                    }
+                }
+            }
+    }
+    body.appendChild(tbl);
+}
--- TW_map/taiwan_map.js
+++ TW_map/taiwan_map.js
@@ -56,47 +56,27 @@ const TaiwanMap = new Vue({
               var twDic = JSON.parse(localStorage.getItem("twDic")); // get tw data
               var cases = twDic[d.properties.COUNTYNAME];
               cases = parseInt(cases, 10);
-              /* 
-              #ea0000
-              #f9331d
-              #ff593f
-              #ff7963
-              #92d2f5
-              #66a5df
-              #3c7ac4
-              #0051a6
-              */
               if(cases >= 3000){
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(112,0,25,255)";
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#ea00008C";
               }else if(cases >= 700){
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(193,0,21,255)";
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#f9331d8C";
               }else if(cases >= 500){
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(228,86,62,255)";
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#ff593f8C";
               }else if(cases >= 200){
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(255,162,119,255)";
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#ff79638C";
               }else if(cases >= 100){
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(254,219,199,255)";
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#92d2f58C";
               }else if(cases >= 50){
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#66a5df8C";
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(205,228,243,255)";
               }else if(cases >= 25){
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#3c7ac48C";
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(205,228,243,255)";
               }else{
                 document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "#0051a68C";
-                // document.getElementById('city' + d.properties.COUNTYCODE).style.fill = "rgba(137,197,228,255)";
               }
             })
             .on('click', d => {
               var twDic = JSON.parse(localStorage.getItem("twDic")); // get tw data
-              console.log(twDic);
-              console.log(twDic[d.properties.COUNTYNAME]);
-
+              
               this.h1 = d.properties.COUNTYNAME + twDic[d.properties.COUNTYNAME]; // 換中文名
               this.h2 = d.properties.COUNTYENG; // 換英文名
               // 有 .active 存在，就移除 .active
